eaf0f25a82ede1ff1c6070148ac3355f4349a65d
nastiadgaf <nastiadgaf@gmail.com>
2022-02-01 14:57:30 +0200

    Add method that creates table

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ class Dropdown {
 		modal.classList.add('hidden');
 	}
 
+
 }
 // const dropdownSelectFont = new Dropdown('select-font');
 // const dropdownSelectSize = new Dropdown('select-size');
@@ -143,6 +144,38 @@ const closeInput = new InputModal('sign-out')
 class TableModal extends Dropdown{
 	constructor (listId) {
 		super(listId);
+		this.main = document.querySelector('#main');
+		this.createTableBtn = document.querySelector('[data-action="create-table"]');
+		this.countTr = document.querySelector('#count-tr');
+		this.countTd = document.querySelector('#count-td');
+		this.widthTd = document.querySelector('#width-td');
+		this.heightTd = document.querySelector('#height-td');
+		this.borderWidth = document.querySelector('#border-width');
+		this.borderStyle = document.querySelector('#select_border');
+		this.borderColor = document.querySelector('#select_border-color');
+
+		this.createTableBtn.addEventListener('click', (e) => {
+			e.preventDefault();
+			this.createTable();
+		})
+	}
+
+	createTable(){
+		const newTable = document.createElement('table');
+
+		for(let i = 0; i < this.countTr.value; i++){
+			let tr = document.createElement('tr');
+			for(let j = 0; j < this.countTd.value; j++) {
+				let td = document.createElement('td');
+				td.textContent = 'td'
+				td.style.width = `${this.widthTd.value}px`;
+				td.style.height = `${this.heightTd.value}px`;
+				td.style.border = `${this.borderWidth.value}px ${this.borderStyle.value} ${this.borderColor.value}`;
+				tr.append(td)
+			}
+			newTable.append(tr);
+		}
+		main.append(newTable);
 	}
 	
 }
