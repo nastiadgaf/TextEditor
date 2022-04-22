536c93b64121d247d226d476d7c9a2d822855d7f
nastiadgaf <nastiadgaf@gmail.com>
2022-04-22 14:10:41 +0300

    Add changing inner modals functional

--- components/dropdown.js
+++ components/dropdown.js
@@ -1,45 +1,48 @@
-
 class Dropdown {
-	constructor(listId) {
-		this.list = document.getElementById(listId);
-		this.toggleModalBtn = document.querySelector(`[data-modal="${listId}"]`);
-		this.closeModalBtn = document.querySelectorAll(`[data-close="${listId}"]`);
-		this.toggleModalBtn.addEventListener('click', ({target: {dataset: {relatedModal}}}) => {
-			this.toggleModal(relatedModal);
-		});
+  constructor(listId) {
+    this.list = document.getElementById(listId);
+    this.toggleModalBtn = document.querySelector(`[data-modal="${listId}"]`);
+    this.closeModalBtn = document.querySelectorAll(`[data-close="${listId}"]`);
+    this.toggleModalBtn.addEventListener(
+      'click',
+      ({
+        target: {
+          dataset: { relatedModal },
+        },
+      }) => {
+        this.toggleModal(relatedModal);
+      }
+    );
 
-		this.closeModalBtn.forEach(btn => {
-			btn.addEventListener('click', ({target: {dataset: {relatedModal}}}) => {
-			this.closeModal(relatedModal);
-		});
-		})
-		
-	}
-	toggleModal = (modalSelector) => {
-		const modals = document.querySelectorAll('[data-modal-panel="main"]');
-		modals.forEach(item => {
-			item.classList.add('hidden');
-		})
-		let modal = document.getElementById(modalSelector);
-		modal.classList.toggle('hidden');
-		
-	}
+    this.closeModalBtn.forEach((btn) => {
+      btn.addEventListener(
+        'click',
+        ({
+          target: {
+            dataset: { relatedModal },
+          },
+        }) => {
+          this.closeModal(relatedModal);
+        }
+      );
+    });
+  }
+  toggleModal = (modalSelector) => {
+    const modals = document.querySelectorAll('[data-modal-panel="main"]');
+    modals.forEach((item) => {
+      item.classList.add('hidden');
+    });
+    let modal = document.getElementById(modalSelector);
+    modal.classList.toggle('hidden');
+  };
 
-	closeModal = (modalSelector) => {
-			let modal = document.getElementById(modalSelector);
-			modal.classList.add('hidden');
-		
-	}	
-
+  closeModal = (modalSelector) => {
+    let modal = document.getElementById(modalSelector);
+    modal.classList.add('hidden');
+  };
 }
 const dropdownSelectFont = new Dropdown('select-font');
 const dropdownSelectSize = new Dropdown('select-size');
 
-
 const dropdownBgColor = new Dropdown('bg-modal');
 const dropdownColor = new Dropdown('color-modal');
-
-const dropdownInnerBgImgModal = new Dropdown('inner-bg-img-modal');
-const dropdownInnerBgColorModal = new Dropdown('inner-bg-color-modal');
-
-
---
+++ components/inner-modals.js
@@ -0,0 +1,29 @@
+class InnerModals {
+  constructor(colorModalAttribute, imgModalAttribute) {
+    this.colorModal = document.querySelector(`#${colorModalAttribute}`);
+    this.imageModal = document.querySelector(`#${imgModalAttribute}`);
+
+    document
+      .querySelector(`[data-modal="${imgModalAttribute}"]`)
+      .addEventListener('click', () => {
+        this.changePanels(this.colorModal, this.imageModal);
+      });
+
+    document
+      .querySelector(`[data-modal="${colorModalAttribute}"]`)
+      .addEventListener('click', () => {
+        this.changePanels(this.imageModal, this.colorModal);
+      });
+    console.log(1);
+  }
+
+  changePanels = (panel, anotherPanel) => {
+    panel.classList.add('hidden');
+    anotherPanel.classList.remove('hidden');
+  };
+}
+
+const innerModal = new InnerModals(
+  'inner-bg-color-modal',
+  'inner-bg-img-modal'
+);
